7999505b9b1b32b22c96a35fafd234e803818484
Chris Horne <lahwran@lahwran.net>
2014-11-11 08:38:02 -0700

    update stuff

--- dotfiles/os_specific/mac.py
+++ dotfiles/os_specific/mac.py
@@ -116,8 +116,13 @@ def p_output(*args, **kwargs):
     except subprocess.CalledProcessError as e:
         return e.output
 
-p_call = subprocess.call
-p_check = subprocess.check_call
+def p_call(command, *a, **kw):
+    logger.info("call: %s", " ".join((x if " " not in x else repr(x)) if len(x) < 35 else repr(x[:33] + "...") for x in command))
+    return subprocess.call(command, *a, **kw)
+
+def p_check(command, *a, **kw):
+    logger.info("check: %s", " ".join((x if " " not in x else repr(x)) if len(x) < 35 else repr(x[:33] + "...") for x in command))
+    return subprocess.check_call(command, *a, **kw)
 
 
 def customize():
@@ -186,10 +191,10 @@ def customize():
                             "LoadPrefsFromCustomFolder", True)
 
     # time until initial key repeat
-    n_r |= set_defaults("-g", "InitialKeyRepeat", 35)
+    n_r |= set_defaults("-g", "InitialKeyRepeat", 25)
 
     # time between key repeats
-    n_r |= set_defaults("-g", "KeyRepeat", 1)
+    n_r |= set_defaults("-g", "KeyRepeat", 2)
 
     # set natural scroll - not apple's "unnatural" scroll
     n_r |= set_defaults("-g", "com.apple.swipescrolldirection", False)
@@ -207,6 +212,9 @@ def customize():
     # Disable window animations - they royally screw up window management apps, and are
     # fairly slow on most apps to boot
     n_r |= set_defaults("-g", "NSAutomaticWindowAnimationsEnabled", False, typed=True)
+
+    # take a while to move windows between displays, so edge-of-screen stuff isn't annoying
+    n_r |= set_defaults("com.apple.dock", "workspaces-edge-delay", 4.0, typed=True)
 
     n_r |= set_defaults("com.apple.screencapture", "location",
             fullpath("~/SpaceMonkey/Screenshots"), typed=True)
@@ -251,20 +259,24 @@ def customize_root():
     p_call(["pmset", "-c", "sleep", "0"])
     p_call(["pmset", "-u", "sleep", "0"])
 
+    # DISABLED (was causing issues with session loss):
     # go into standby mode fairly soon once in sleep, because
     # standby mode allows tossing the filevault key
-    p_call(["pmset", "-a", "standby", "1"])
-    p_call(["pmset", "-a", "standbydelay", "300"]) # seconds
-    p_call(["pmset", "-a", "autopoweroff", "1"])
-    p_call(["pmset", "-a", "autopoweroffdelay", "300"]) # seconds
+    p_call(["pmset", "-a", "standby", "0"])
+    # p_call(["pmset", "-a", "standbydelay", "300"]) # seconds
+    p_call(["pmset", "-a", "autopoweroff", "0"])
+    # p_call(["pmset", "-a", "autopoweroff", "5"])
+
     # there's a big flashy warning about this option in the manual page
-    # for pmset. 25 is a supported value.
+    # for pmset. 25 is a supported value, that empties ram.
     # setting this assumes hibernatefile is set, which it seems to be by
     # default.
-    p_call(["pmset", "-a", "hibernatemode", "25"])
+    # DISABLED - was causing issues with session loss.
+    p_call(["pmset", "-a", "hibernatemode", "0"]) #"25"])
     # once the computer goes into hibernate mode, unload filevault. to resume
     # from this requires _two_ passwords, filevault and login.
-    p_call(["pmset", "-a", "destroyfvkeyonstandby", "1"])
+    # DISABLED - was causing issues with session loss.
+    p_call(["pmset", "-a", "destroyfvkeyonstandby", "0"])
 
 
 def find_file(name):
@@ -344,7 +356,7 @@ def disable_default_escape_action():
         else:
             productids.append(value)
     product_pairs = zip(vendorids, productids)
-    print "Keyboard product pairs:", product_pairs
+    logger.debug("Keyboard product pairs: %r", product_pairs)
 
     # this maps from 0 (escape) to -1 (no action), and disables any other
     # osx-internal modifier key mappings
