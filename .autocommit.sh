8f3fc1ba7f77e301be6f7574a86206936fefdaef
Chris Horne <lahwran0@gmail.com>
2017-08-23 13:46:04 -0700

    autocommit

--- .autocommit.sh
+++ .autocommit.sh
@@ -6,7 +6,7 @@ git commit -m "autocommit"
 git config merge.ff only
 bin/check_run
 
-if [ "$(hostname)" = "lahwran-u08" ]; then
+if [ "$(hostname)" = "lahwran-u05" ]; then
     git merge laptop/master 2>&1 >> WARNING_GIT_OUT_OF_SYNC && rm WARNING_GIT_OUT_OF_SYNC
 else
     git push desktop master:laptop/master
